cf8458ff8a3f5ebafa14908e3effb94526680f9c
Viorel Lazari <vlazari@gmail.com>
2022-02-10 14:38:42 +0100

    extra validation

--- js/question-2.js
+++ js/question-2.js
@@ -5,24 +5,27 @@ const loader = document.querySelector(".load");
 async function getGames() {
 	try {
 		const response = await fetch(url);
-
 		const games = await response.json();
 		const gameList = games.results;
 		loader.style.display = "none";
 
 		for (let i = 0; i < 8; i++) {
 			let rating = gameList[i].rating;
+			let tags = gameList[i].tags.length;
+			let gameClass = "game";
 			if (isNaN(Number(rating)) || !rating) {
 				rating = "not rated";
 			}
-			let gameClass = "game";
 			if (i % 2 === 0) {
 				gameClass = "game altLine";
+			}
+			if (!tags) {
+				tags = 0;
 			}
 			container.innerHTML += `<div class="games">
                                 <div class="${gameClass}">${gameList[i].name}</div> 
                                 <div class="${gameClass}">${rating}</div>
-                                <div class="${gameClass}">${gameList[i].tags.length}</div>
+                                <div class="${gameClass}">${tags}</div>
                               </div>`;
 		}
 		console.log("success");
